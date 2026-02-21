093c7ab4592aa178c6aa3398193a5afae995d47c
“lasiaf” <muhammadfaisal.uns@gmail.com>
2026-02-21 22:15:29 +0700

    update usecase

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-const data = 'Hi';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Halo selamat datang</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
--- src/domain/usecases/index.js
+++ src/domain/usecases/index.js
@@ -1,1 +1,20 @@
 const data = "Hi";
+const data2 = "Hi";
+/*
+1. Authentications : auth.js
+    a. Login user
+    b. Tampilkan Identitas User
+    c. Logout user
+2. Data Master : master.js
+    a. Simpan Data Master ke Localstorage
+    b. Tampilkan Data Master
+    c. Tampilkan daftar Kegiatan
+3. Activity : activity.js
+    a. Cari dan Pilih Blok Tanam
+    b. Input data Tanam
+    c. Edit data Tanam
+    d. Hapus data Tanam
+    e. Lihat daftar data Tanam
+    f. Kirim data Tanam ke server
+
+*/
